02360442e1941f488eda0f85d70b0a57594babf7
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2021-03-31 15:48:40 -0300

    Create rotativo.py

---
+++ rotativo.py
@@ -0,0 +1,19 @@
+#  Menu
+print('=' * 20 + ' RS ROTATIVO ' + '=' * 20)
+print('\n')
+
+while True:
+    print('-' * 10 + ' MENU ' + '-' * 10)
+
+    print(' Entre com a opção desejada\n'
+          '1 - Gerar entrada de veiculos: ')
+
+    opc = input()
+
+    if opc == '1':
+        print('=' * 20)
+        print('Entrada de veiculos')
+        print('-' * 20)
+    else:
+        break
+
