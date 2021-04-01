06014bcea6251498225ca4c1e2779f3902f98a39
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2021-04-01 18:11:01 -0300

    Update rotativo.py

--- rotativo.py
+++ rotativo.py
@@ -1,3 +1,14 @@
+rotativo = [{'Placa': '1234', 'Modelo': 'dk150', 'Cor': 'vermelha'}]
+
+
+def entrada(placa_car, modelo_car, cor_car):
+    user = {'Placa': placa_car, 'Modelo': modelo_car, 'Cor': cor_car}
+
+    for i in rotativo:
+        for b in i:
+            return b
+
+
 #  Menu
 print('=' * 20 + ' RS ROTATIVO ' + '=' * 20)
 print('\n')
@@ -14,6 +25,13 @@ while True:
         print('=' * 20)
         print('Entrada de veiculos')
         print('-' * 20)
+
+        placa = input('Informe a placa: ')
+        modelo = input('Informe o modelo: ')
+        cor = input('Informe a cor: ')
+
+        print(entrada(placa, modelo, cor))
+
     else:
         break
 
