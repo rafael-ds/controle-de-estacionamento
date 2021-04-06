831ba98f6695f9e31ac9594c99b639ec8c1f5f2a
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2021-04-06 18:04:56 -0300

    Update rotativo.py

--- rotativo.py
+++ rotativo.py
@@ -1,37 +1,46 @@
-rotativo = [{'Placa': '1234', 'Modelo': 'dk150', 'Cor': 'vermelha'}]
+vagas_rotativo = []
 
 
-def entrada(placa_car, modelo_car, cor_car):
-    user = {'Placa': placa_car, 'Modelo': modelo_car, 'Cor': cor_car}
+def cadastro_rotativo(placa, modelo, cor):
+    """
+    Tem como objetico adquiri os dados do veiculo
 
-    for i in rotativo:
-        for b in i:
-            return b
+    :param placa: Pede a placa do veiculo
+    :param modelo: pede o modelo do veiculo
+    :param cor: Pede a cor predominante do veiculo
+    :return: dados
+    """
+    dados = {'Placa': placa, 'Modelo': modelo, 'Cor': cor}
+    return dados
 
 
-#  Menu
-print('=' * 20 + ' RS ROTATIVO ' + '=' * 20)
-print('\n')
+def dados_veic_rotat():
+    enter_placa = input('Informe a placa do veiculo: ')
+    enter_modelo = input('Informe o modelo do veiculo: ')
+    enter_cor = input('Informe a cor do veiculo: ')
+
+    cadastro = cadastro_rotativo(enter_placa, enter_modelo, enter_cor)  # cadastro --> armazena a função
+    # cadastro_rotativo() recebendo como argumento
+    # os dados do veiculo informado pelo usuario
+    vagas_rotativo.append(cadastro)  # adiciona a lista vaga_rotativo o os dados informado pelo usuario
+
 
 while True:
-    print('-' * 10 + ' MENU ' + '-' * 10)
+    opc = input('Digite (1) para entra ou (2) para sair: ')
+    if opc != '2':
+        placa_car = input('Placa: ')
+        dado = list(filter(lambda p: p['Placa'] == placa_car, vagas_rotativo))  # pecorre a lista vaga_rotativo em
+        # comparando o placa_car com dado
 
-    print(' Entre com a opção desejada\n'
-          '1 - Gerar entrada de veiculos: ')
-
-    opc = input()
-
-    if opc == '1':
-        print('=' * 20)
-        print('Entrada de veiculos')
-        print('-' * 20)
-
-        placa = input('Informe a placa: ')
-        modelo = input('Informe o modelo: ')
-        cor = input('Informe a cor: ')
-
-        print(entrada(placa, modelo, cor))
-
+        if dado:
+            print(dado)
+        else:
+            um = input('A placa informada não se encontra cadastrada: \npara cadastra-la pressione (1)')
+            if um:
+                dados_veic_rotat()
+            else:
+                break
     else:
         break
 
+print(vagas_rotativo)
